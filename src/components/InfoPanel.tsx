6b8f523ac696d14950b1bd7f1c8f0fd2ec32ac65
Alan Burnett <aburnettt@netnumber.com>
2021-02-28 11:41:36 -0500

    Changed Datagrid to Table for abilities

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -4,7 +4,14 @@ import {
     Button,
     Box
 } from '@material-ui/core';
-import { DataGrid, RowsProp, ColDef, CellParams } from '@material-ui/data-grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 
@@ -29,68 +36,63 @@ export default class InfoPanel extends React.Component<IProps, IState> {
 
 
     render() {
-        const buffRows: RowsProp = [];
-        const buffColumns: ColDef[] = [];
 
 
 
-        const abilityRows: RowsProp = [];
-        const abilityColumns: ColDef[] = [
-            { field: 'name', headerName: 'Name', flex: 45 },
-            { field: 'action', headerName: 'Action', flex: 30 },
-            { field: 'atk', headerName: 'Atk', flex: 20 },
-            {
-                field: 'effect', headerName: 'Effect', flex: 85,
-                renderCell: (params: CellParams) => (
-                    <strong>
-                        {(params.value as string).split("|||")[0]}
-                        <Tooltip title={(params.value as string).split("|||")[1]} arrow interactive>
-                            <Button>?</Button>
-                        </Tooltip>
-                    </strong>
-                )
-            }
-        ];
 
-        for (let i = 0; i < this.props.abilities.length; i++) {
-            var a = this.props.abilities[i];
-            abilityRows.push(
-                {
-                    id: i,
-                    name: a["name"],
-                    action: a["action"],
-                    atk: a["atk"],
-                    effect: a["dmg"] + " " + a["effect"] + "|||" + a["detail"].replaceAll("\"", "")
-                }
-            )
+        var abilityHeaders: string[] = [
+            "Action",
+            "Atk",
+            "Effect"
+        ]
 
-        }
         return (
             <Box display="flex">
                 <Box minHeight="25%" width="30%" position="Left" display="flex">
                     <div style={{ width: '500px' }}>
                         <h3>Attributes</h3>
-                        <DataGrid
-                            pageSize={15}
-                            autoHeight={true}
-                            hideFooter={true}
-                            hideFooterPagination={true}
-
-                            rows={buffRows}
-                            columns={buffColumns} />
                     </div>
                 </Box>
                 <Box minHeight="25%" width="65%" position="right" display="flex">
                     <div style={{ width: '800px' }}>
                         <h3> Abilities</h3>
-                        <DataGrid
-                            pageSize={15}
-                            autoHeight={true}
-                            hideFooter={true}
-                            hideFooterPagination={true}
 
-                            rows={abilityRows}
-                            columns={abilityColumns} />
+                        <TableContainer component={Paper}>
+                            <Table aria-label="simple table">
+                                {/*
+                                Looks better without header IMO
+                                <TableHead>
+                                    <TableRow>
+                                        {abilityHeaders.map((header, i) => {
+                                            return (<TableCell><strong>{header}</strong></TableCell>)
+                                        })}
+
+                                    </TableRow>
+                                </TableHead>
+                                    */}
+                                {this.props.abilities.map((a, i) => {
+                                    return (<TableBody>
+                                        <TableRow><strong>{a["name"]}</strong></TableRow>
+                                        <TableRow>
+                                            <TableCell>
+                                                {a["action"]}
+                                            </TableCell>
+                                            <TableCell>
+                                                {a["atk"]}
+                                            </TableCell>
+                                            <TableCell>
+                                                 {a["dmg"] + " " + a["effect"]} 
+                                                {a["detail"] && a["detail"].length > 0 && (
+                                                    <Tooltip title={a["detail"].replaceAll("\"", "")} arrow interactive>
+                                                        <Button>(?)</Button>
+                                                    </Tooltip>
+                                                )}
+                                            </TableCell>
+                                        </TableRow></TableBody>)
+                                })}
+                            </Table>
+                        </TableContainer>
+
                     </div>
                 </Box>
             </Box >
